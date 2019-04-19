556044f0c9aaff1516d2f002c96c07c641607af5
girish <giridevelop@gmail.com>
2019-04-19 23:29:05 +0530

    react-app-custom-component

--- rcg/src/App.js
+++ rcg/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //        <h1>Welcom to react Learning</h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+           <h1>Welcom to react Learning</h1>
+           <Person />
+      </div>
+    );
     // return React.createElement('div','null','h1','welcome to react learning')
-    return React.createElement('div',{className: 'App'},React.createElement('h1','null','welcome to react learning'));
+    //return React.createElement('div',{className: 'App'},React.createElement('h1','null','welcome to react learning'));
   }
 }
 
---
+++ rcg/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const person = () => {
+  return <p>my custom component</p>
+};
+
+export default person;
