bcce7379e246ac14b002eee0157ad433a212ad1e
girish <giridevelop@gmail.com>
2019-04-19 23:45:43 +0530

    session-3-ouput-dynamic-content

--- rcg/src/Person/Person.js
+++ rcg/src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const person = () => {
-  return <p>my custom component</p>
+  return <p>my custom component and my age is {Math.floor(Math.random()*20)}</p>
 };
 
 export default person;
